a53528fac7d901fa9186930f592c2ceeac09761f
Virajbiz98 <Virajbiz98@gmail.com>
2024-10-28 03:34:15 +0530

    Update doc.py

--- doc.py
+++ doc.py
@@ -1,7 +1,5 @@
-# Insert a document
-data = {"name": "shadow", "email": "shadow90@example.com"}
-collection.insert_one(data)
-
-# Find a document
-result = collection.find_one({"name": "shadow"})
-print(result)
+# update one matching document
+collection.update_one(
+    {"name":"shadow"}, # filter (what dofind)
+    {"$set": {"email": "updateemail@example.com"}}
+)
