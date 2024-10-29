e178b9fd2585ac3e7805950a3918c524f1f28660
Virajbiz98 <Virajbiz98@gmail.com>
2024-10-29 18:28:44 +0530

    Update doc.py

--- doc.py
+++ doc.py
@@ -1,31 +1,26 @@
-# insert document
-document = {
-        "id"  : {"$oid":"sampathtile123"},  # this one:- {"$oid":".."} must need for insert id
-        "name" : "sampath ",
-        "email": "sampath94@taile.com",
-        "age"  : 30,
-        "address":{
-            "street": "365 pliyandala ",
-            "city"  : "werahara",
-            "state" : "western",
-            "zip"   : "94345"
-        },
-        "phone numbers":[
-            {
-                "type" : "home",
-                "number": "0413497017"
-            },
-            {
-                "type" : "work",
-                "number": "0765534223"
-            }
-        ],
+# Test the connection
+try:
+    client.admin.command('ping')
+    print("Successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
-        "is active": True, #  isActive to True means the user is currently active, while False would indicate they are inactive.
-        "createdAT": datetime.now(), #.  This field stores the date and time when the document (or entity) was created.
-        "updateAT" : datetime.now()# This field records the date and time when the document was last updated.
-            }
-        
+db = client["first_project"]
+collection = db["dev_02"]
+
+#  insert document
+document = {
+    "name":"max",
+    "email":"max45@max.comb",
+    "mobile": "01993494",
+    "work" : "nojob",
+    "address":{
+        "street":"3434 jafna",
+        "city"  : "jafna",
+        "state" : "easten",
+        "zip"   : "188938"
+    }
+    }
 
 result = collection.insert_one(document)
-print(result)
+print(result.inserted_id)
