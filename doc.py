128994efcdeaa9e9dc8bbf67ca861a105242de54
Virajbiz98 <Virajbiz98@gmail.com>
2024-10-29 15:14:20 +0530

    Update doc.py

--- doc.py
+++ doc.py
@@ -1,5 +1,31 @@
-# Insert a document
-data = {"name": "test user", "email": "testuser@example.com"}
-collection.insert_one(data)
+# insert document
+document = {
+        "id"  : {"$oid":"sampathtile123"},  # this one:- {"$oid":".."} must need for insert id
+        "name" : "sampath ",
+        "email": "sampath94@taile.com",
+        "age"  : 30,
+        "address":{
+            "street": "365 pliyandala ",
+            "city"  : "werahara",
+            "state" : "western",
+            "zip"   : "94345"
+        },
+        "phone numbers":[
+            {
+                "type" : "home",
+                "number": "0413497017"
+            },
+            {
+                "type" : "work",
+                "number": "0765534223"
+            }
+        ],
 
-collection.delete_one({"name": "test user"})
+        "is active": True, #  isActive to True means the user is currently active, while False would indicate they are inactive.
+        "createdAT": datetime.now(), #.  This field stores the date and time when the document (or entity) was created.
+        "updateAT" : datetime.now()# This field records the date and time when the document was last updated.
+            }
+        
+
+result = collection.insert_one(document)
+print(result)
