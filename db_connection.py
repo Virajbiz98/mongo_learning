b8a45ebacde0c4c2599d22c1f3665ef8df02e01a
K.D.M. Jayawardhana <malithjayawardhana@gmail.com>
2024-10-29 12:41:46 +0000

    #2 test the connection and obtaning the data from tne collection

--- db_connection.py
+++ db_connection.py
@@ -1,10 +1,22 @@
 import pymongo 
 from pymongo import MongoClient
-
 import datetime
-
 MONGO_URL = "mongodb+srv://admin:dev_testing@cluster0.g21xt.mongodb.net/"
 
 client = MongoClient(MONGO_URL)
-db = client['sample_mflix']
-print(db)
+db = client['dev_mongo']
+collection = db['mongo_dev']
+
+#Document = document_collection['Documents']       # this one is not needed 
+
+print(collection)
+
+query = {"name": "isuru"}
+document = collection.find_one(query)
+
+
+# Print the found document
+if document:
+    print(document)
+else:
+    print("Document not found.")
