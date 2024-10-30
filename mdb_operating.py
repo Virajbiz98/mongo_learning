7ae96d48d41c477eeddc501637903e047b817d25
Virajbiz98 <Virajbiz98@gmail.com>
2024-10-30 11:00:54 +0530

    Update mdb_operating.py

--- mdb_operating.py
+++ mdb_operating.py
@@ -3,8 +3,25 @@ query = {"name":"isuru"}
 collection.insert_one(query)
 
 # how to insert many query
-query = [{"name":"isuru"},{"name":"eranda"}]
-colection.insert_many(query)
+import pymongo
+
+# Your MongoDB connection string
+client = pymongo.MongoClient("mongodb+srv://DEVMONGO:DEVMONGO@cluster0.wcbtj.mongodb.net/")
+
+# Test the connection
+try:
+    client.admin.command('ping')
+    print("Successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+db = client["first_project"]
+collection = db["dev_02"]
+
+
+query = [{"name":"free"},{"name":"learning"}]
+collection.insert_many(query)
+
 
 # how to find query
 result = collection.find_one({"name":"isuru"})
